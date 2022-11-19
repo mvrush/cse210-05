6951120ee77e347e7f731ee3a9097694f6d3c842
mvrush <mvrush@gmail.com>
2022-11-18 21:37:42 -0700

    Make big changes to cycle2 class

--- cycle/cycle/game/casting/cycle2.py
+++ cycle/cycle/game/casting/cycle2.py
@@ -3,7 +3,7 @@ from game.casting.actor import Actor
 from game.casting.cycle1 import Cycle1
 from game.shared.point import Point
 
-
+# This is a derived class from the Cycle1() class. So we don't need to duplicate all methods in this class.
 class Cycle2(Cycle1):
     """
     A long limbless reptile.
@@ -15,25 +15,7 @@ class Cycle2(Cycle1):
     """
     def __init__(self):
         super().__init__() # This gives us access to all variables and methods in the Actor() class
-        self._segments = []
-        self._prepare_body()
 
-    def get_segments(self):
-        return self._segments
-
-    def move_next(self):
-        # move all segments
-        for segment in self._segments:
-            segment.move_next()
-        # update velocities
-        for i in range(len(self._segments) - 1, 0, -1):
-            trailing = self._segments[i]
-            previous = self._segments[i - 1]
-            velocity = previous.get_velocity()
-            trailing.set_velocity(velocity)
-
-    def get_head(self):
-        return self._segments[0]
 
     def grow_tail(self, cast, number_of_segments): # Receives the instance of 'cast' and 'number_of_segments' values from 'snake.grow_tail(cast, 1)' found in draw_actors_action.py
         for i in range(number_of_segments):
